3ec0b36be172b96bbbb53f4c692c6187e5d34392
sadak hassan <sadaqsamor99@gmail.com>
2025-08-27 14:55:30 +0300

    pyt Tutt.

--- python-course-dugsiiye-5a32d9287d67285f29f4585a69f591dda93905a5/All Python Practices/2-distance-converter-program.py
+++ python-course-dugsiiye-5a32d9287d67285f29f4585a69f591dda93905a5/All Python Practices/2-distance-converter-program.py
@@ -3,10 +3,10 @@ while True:
     choice = input('(MI) for miles, (KM) for kilometers & (q) for quit: ').lower()
     if choice == 'q':
         break
-    elif choice == 'mi':
+    elif choice == 'mile':
         km = int(input('Enter the kilometers: '))
-        mi = km / 1.609
-        print(f'{km} killometers is {mi} miles')
+        mile = km / 1.609
+        print(f'{km} killometers is {mile} miles')
     elif choice == 'km':
         mi = int(input('Enter the miles: '))
         km = mi * 1.609
