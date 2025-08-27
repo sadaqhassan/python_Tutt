f1adc2ac4cc179b313370b54f4aed4a69413af6c
sadak hassan <sadaqsamor99@gmail.com>
2025-08-27 14:56:06 +0300

    pyt Tutt

--- python-course-dugsiiye-5a32d9287d67285f29f4585a69f591dda93905a5/Chapter 1 Introduction to Python/Code/main.py
+++ python-course-dugsiiye-5a32d9287d67285f29f4585a69f591dda93905a5/Chapter 1 Introduction to Python/Code/main.py
@@ -1,103 +1,103 @@
-# # # name = value
+# # name = value
 
-# # # int
-
-# # age = 34
-
-# # # float
-
-# # score = 99.7
-
-# # # string
-
-# # name = 'Sharafdin'
-# # age = '123'
-
-# player1 = True
-# player2 = False
-
-
-# name = input('What is your name: ')
-# print('Your name: ' + name)
-
-# print('Jawaabtu waa: ', 34)
-
-# print('Sharafdin')
-
-# num1 = int(input('Number 1:'))
-
-# int()
-# str()
-# bool()
-# num1 = int(input('Number 1:'))
-
-# num2 = int(input('Number 2:'))
-
-# print('Jawaabtu waa: ', num1 + num2)
-
-# print(type(num1), type(num2))
-
-# name = input('Name: ')
-
-# name = int(input('Name: '))
-
-# print(type(name))
-# str(age = 34)
+# # int
 
 # age = 34
 
-# type = type(str(age))
+# # float
 
-# print(type)
+# score = 99.7
 
-# first_name = 'Mr'
-# last_name = ' Sharafdin'
-
-# print(first_name + last_name)
-
-# name = input('Your name: ')
-# age = input('Your Age: ')
-
-# print('Your name is ' + name + ' and your age is ' + age)
+# # string
 
 # name = 'Sharafdin'
+# age = '123'
 
-# num = '12345678'
+player1 = True
+player2 = False
 
-# []
 
-# 0123
+name = input('What is your name: ')
+print('Your name: ' + name)
 
-# print(num[1::2])
+print('Jawaabtu waa: ', 34)
 
-# name = input('Name: ')
-# age = int(input('Age: '))
+print('Sharafdin')
 
-# print('Your name is ' + name + ' and your age is ' + age)
+num1 = int(input('Number 1:'))
 
-# print(f"Your name is {name} and your age is {type(age)}")
+int()
+str()
+bool()
+num1 = int(input('Number 1:'))
 
-# name = 'mr sharafdin'
+num2 = int(input('Number 2:'))
 
-# print(name.title())
+print('Jawaabtu waa: ', num1 + num2)
 
-# s = 'Hello World Yow!'
+print(type(num1), type(num2))
 
-# print(s.split(' '))
+name = input('Name: ')
 
-# name = 'Isa Yusuf Sharafdin'
+name = int(input('Name: '))
 
-# namelist = name.split(' ')
+print(type(name))
+str(age = 34)
 
-# print('Your last name:', namelist[-1])
+age = 34
 
-# name = input('Enter your name: ')
-# password = input('Enter your password: ')
+type = type(str(age))
 
-# print('ASC, ' + name + 'Your password ' + password + ' is ' + len(password) + ' letters long.')
+print(type)
 
-# hidden_password = '*' * len(password)
+first_name = 'Mr'
+last_name = ' Sharafdin'
 
-# len_pass = len(password)
+print(first_name + last_name)
 
-# print(f'ASC, {name} your password {hidden_password} is {len_pass} letters long')
+name = input('Your name: ')
+age = input('Your Age: ')
+
+print('Your name is ' + name + ' and your age is ' + age)
+
+name = 'Sharafdin'
+
+num = '12345678'
+
+[]
+
+0123
+
+print(num[1::2])
+
+name = input('Name: ')
+age = int(input('Age: '))
+
+print('Your name is ' + name + ' and your age is ' + age)
+
+print(f"Your name is {name} and your age is {type(age)}")
+
+name = 'mr sharafdin'
+
+print(name.title())
+
+s = 'Hello World Yow!'
+
+print(s.split(' '))
+
+name = 'Isa Yusuf Sharafdin'
+
+namelist = name.split(' ')
+
+print('Your last name:', namelist[-1])
+
+name = input('Enter your name: ')
+password = input('Enter your password: ')
+
+print('ASC, ' + name + 'Your password ' + password + ' is ' + len(password) + ' letters long.')
+
+hidden_password = '*' * len(password)
+
+len_pass = len(password)
+
+print(f'ASC, {name} your password {hidden_password} is {len_pass} letters long')
